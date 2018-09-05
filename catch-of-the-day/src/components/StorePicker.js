08c88a6668c6af92044ffc197367bc5978120812
Saikat Das <saikatdas0790@gmail.com>
2018-09-05 08:48:26 +0530

    Add App, Store Picker, Header, Order and Inventory

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <Fragment>
+                <p>Fish!</p>
+        <form action="" className="store-selector">
+                    <h2>Please enter a store</h2>
+          <input type="text" name="" id="" required placeholder="Store Name" />
+          <button type="submit">Visit Store ➡️ </button>
+                </form>
+      </Fragment>
+    );
+  }
+}
+
+export default StorePicker;
